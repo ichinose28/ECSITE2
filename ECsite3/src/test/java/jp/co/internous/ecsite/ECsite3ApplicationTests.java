b1fc1fc54147b284a4cafab80227ab028856e523
ichinose28 kousuke <sool931bullet@gmail.com>
2023-12-26 23:57:56 +0900

    課題

---
+++ ECsite3/src/test/java/jp/co/internous/ecsite/ECsite3ApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.internous.ecsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECsite3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
